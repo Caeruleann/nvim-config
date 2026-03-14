b33bbe9a68532535f57f5297cf271cbed9f4674e
Caeruleann <wanghairui0927@gmail.com>
2026-03-14 16:10:40 +0800

    treesitter update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,27 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    -- 确保 python 在这个列表里
-    ensure_installed = { "lua", "vim", "vimdoc", "python", "c", "cpp" },
-    -- 必须显式开启 highlight
-    highlight = {
-      enable = true,
-      -- 如果你发现某些高亮冲突，可以尝试设为 false
-      additional_vim_regex_highlighting = false,
-    },
-    -- 建议同时开启缩进
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    -- 注意：针对新版 nvim-treesitter，如果仍报错找不到 configs，
-    -- 请确保你没有手动 require("nvim-treesitter.configs")
-    -- 直接使用下面的方式（适配大多数版本）：
-    local status, configs = pcall(require, "nvim-treesitter.configs")
-    if status then
-        configs.setup(opts)
-    end
-  end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        -- 1. 保持你原有的配置逻辑
+        local configs = require("nvim-treesitter.config")
+        configs.setup({
+            ensure_installed = { "lua", "python", "c", "cpp", "vim", "vimdoc" },
+            highlight = { 
+                enable = true, 
+            },
+            indent = { enable = true },
+        })
+  
+        -- 2. 【核心修复】手动强制开启高亮引擎
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "python", "lua", "c", "cpp" },
+            callback = function()
+                vim.schedule(function()
+                    if not vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()] then
+                        vim.treesitter.start()
+                    end
+                end)
+            end,
+        })
+    end,
 }
