e2a57f10683650e033e15aa3d3098a6a56b81cf7
Caeruleann <wanghairui0927@gmail.com>
2026-03-15 17:44:55 +0800

    update keymap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,7 +4,11 @@ return {
         dependencies = { 'rafamadriz/friendly-snippets' },
         version = '1.*',
         opts = {
-            keymap = { preset = 'default' },
+            keymap = {
+                ['<C-j>'] = { 'select_next', 'fallback' },
+                ['<C-k>'] = { 'select_prev', 'fallback' },
+                ['<CR>'] = { 'accept', 'fallback' },
+            },
             appearance = { nerd_font_variant = 'mono' },
             completion = { documentation = { auto_show = false } },
             sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
